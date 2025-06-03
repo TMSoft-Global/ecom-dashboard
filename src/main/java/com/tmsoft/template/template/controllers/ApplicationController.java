8fb6025338391e89e7671188e67b4900881de625
Michael kofi Tamakloe <michaeltamekloe18.mt@gmail.com>
2025-06-03 20:54:11 +0000

    done with catalog files

--- src/main/java/com/tmsoft/template/template/controllers/ApplicationController.java
+++ src/main/java/com/tmsoft/template/template/controllers/ApplicationController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @Controller
 public class ApplicationController {
     
@@ -12,4 +13,33 @@ public class ApplicationController {
         return "index";
     }
     
+    @GetMapping("/catalog/products")
+    public String catalogProduct() {
+        return "/catalog/page-products";
+    }
+    
+    @GetMapping("/catalog/add-product")
+    public String catalogAddProduct() {
+        return "/catalog/page-add-product";
+    }
+
+    @GetMapping("/catalog/edit-product")
+    public String catalogEditProduct() {
+        return "/catalog/page-edit-product";
+    }
+
+    @GetMapping("/catalog/categories")
+    public String catalogCategory() {
+        return "/catalog/page-categories";
+    }
+
+    @GetMapping("/catalog/add-category")
+    public String catalogAddCategory() {
+        return "/catalog/page-add-category";
+    }
+
+    @GetMapping("/catalog/edit-category")
+    public String catalogEditCategory() {
+        return "/catalog/page-edit-category";
+    }
 }
